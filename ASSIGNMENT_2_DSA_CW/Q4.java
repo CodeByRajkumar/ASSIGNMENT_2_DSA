f1c4a490ca9a81677a049496dc88d20968dd17ce
Raj Kumar <131976083+CodeByRajkumar@users.noreply.github.com>
2023-07-14 06:19:21 +0530

    Update Q4.java

--- ASSIGNMENT_2_DSA_CW/Q4.java
+++ ASSIGNMENT_2_DSA_CW/Q4.java
@@ -1,23 +1,28 @@
 class product
-
+{
 int pid;
 
 double price; static double tot_price; product(int pid, double price)
 
 this.pid = pid; this.price = price;
 
-} void display()
+}
+void display(){
 
 System.out.println(pid +"\t\t"+ price);
 
-} static double totalAmount(product[] prod)
+}
+static double totalAmount(product[] prod)
+{
+for (int i = 0; i < prod.length; i++) 
+        tot_price += prod[i].price;
+return tot price;
+}
 
-for (int i = 0; i < prod.length; i++) tot_price += prod[i].price; return tot price;
-
-} public class Q4
-
-public static void main(String[] args) {
-
+public class Q4
+{
+public static void main(String[] args) 
+  {
 product[] products = new product[5]; products[0] = new product (785432, 199.99);
 
 products[1] = new product (845753, 79.99);
@@ -31,3 +36,5 @@ for (int i = 0; i < products.length; i++)
 System.out.print( (i+1)+"\t"); products[i].display();
 
 double total_price = product.totalAmount (products); System.out.println("Total price is: + total price+" rupees only...");
+    }
+}
